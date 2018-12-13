6181394bcd80c85ff6324ace54b2d020fe425b6e
chuah suatshan <38532647+suatshan@users.noreply.github.com>
2018-12-13 13:58:21 +0800

    Add files via upload

---
+++ project/src/main/java/interfaceReading.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public interface interfaceReading {
+    String readPDF(String fileName) throws IOException;
+    boolean testRead(String text) throws InterruptedException;
+    int wordCounting(String text) throws InterruptedException;
+    int charCounting(String text) throws InterruptedException;
+    //int getWord(int value);
+}
