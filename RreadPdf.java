153d6a430086e26903911c01bd4ba91ef29aedbb
chuah suatshan <38532647+suatshan@users.noreply.github.com>
2018-12-23 20:15:01 +0800

    Add files via upload

--- RreadPdf.java
+++ RreadPdf.java
@@ -4,11 +4,19 @@ import org.apache.pdfbox.text.PDFTextStripperByArea;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.RecursiveTask;
 
-public class RreadPdf implements interfaceReading {
+public class RreadPdf extends RecursiveTask <Integer> implements interfaceReading {
+
     String text = null;
     int charCount = 0;
     boolean testing = false;
+    Scanner scanner;
+    String word;
+    ArrayList<String> words= new ArrayList<>();
+    int numC, numW;
 
     @Override
     public String readPDF(String fileName) throws IOException {
@@ -21,7 +29,7 @@ public class RreadPdf implements interfaceReading {
                 PDFTextStripper tStripper = new PDFTextStripper();
                 text = tStripper.getText(pdDocument);
 
-                //Clear... Successfully read the fcking file.
+                //Clear... Successfully read
                 //System.out.println(text);
 
                 pdDocument.close();
@@ -30,16 +38,48 @@ public class RreadPdf implements interfaceReading {
         return text;
     }
 
+    public RreadPdf(){
+        //Empty Constructor
+    }
+
+    public RreadPdf (String text){
+        this.text = text;
+    }
+
     @Override
     public boolean testRead(String text) throws InterruptedException {
         if(text == null) {
             System.out.println("The PDF File is Empty! ");
+            System.exit(0);
+            //End the Program
         }else {
             Thread.sleep(1000);
-            //System.out.println("Successfully read the PDF File!");
             testing = true;
         }
         return testing;
+    }
+
+
+
+    public int getWord(String text){
+        try {
+            scanner = new Scanner(text);
+            if (!scanner.hasNext()) {
+                System.out.println("Text is Null.. Try Again");
+                //System.exit(0);
+            } else {
+                //Thread.sleep(1000);
+                while (scanner.hasNext()) {
+                    word = scanner.next();
+                    words.add(word);
+                }//End while Loops
+                //System.out.println(words.size());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //System.out.println(words.size());
+        return words.size();
     }
 
     @Override
@@ -50,7 +90,6 @@ public class RreadPdf implements interfaceReading {
             }
             Thread.sleep(1000);
             String[] words = text.split("\\s+");
-            //System.out.println("Number of Word in PDF file : " + words.length);
             return words.length;
         }
     }
@@ -66,7 +105,37 @@ public class RreadPdf implements interfaceReading {
             }//end
         }//End For
         Thread.sleep(1000);
-        //System.out.println("Number of Character in PDF File : " + charCount);
         return charCount;
+    }
+
+
+    @Override
+    protected Integer compute() {
+        try {
+            if(text != null){
+                //Word Counting
+                String text1 = text;
+                String[] words = text.split("\\s+");
+                numW = words.length;
+
+                String lines[] = text1.split("\\r\n");
+                for (String line : lines) {
+                    //start
+                    String[] word1 = line.split(" ");
+                    for (String word11 : word1) {
+                        charCount += word11.length();
+                    }//end
+                }//End For
+                numC = charCount;
+
+            }else {
+                RreadPdf wordCount = new RreadPdf(text);
+                wordCount.fork();
+                wordCount.join();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return  numW + numC;
     }
 }
