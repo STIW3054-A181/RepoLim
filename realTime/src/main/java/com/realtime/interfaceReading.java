4288ccc12a4275fbdfecab8726e02987ecda0635
Kopi Lim <33795389+kopilim96@users.noreply.github.com>
2018-12-24 11:02:56 +0800

    Final Edition for Real Time Project (Latest)
    Latest 24/12

--- realTime/src/main/java/com/realtime/interfaceReading.java
+++ realTime/src/main/java/com/realtime/interfaceReading.java
@@ -5,8 +5,5 @@ import java.io.IOException;
 public interface interfaceReading {
     String readPDF(String fileName) throws IOException;
     boolean testRead(String text) throws InterruptedException;
-    //int getWord(String text);
-    //int wordCounting(String text) throws InterruptedException;
-    //int charCounting(String text) throws InterruptedException;
-    //int getWord(int value);
+    int[] getWord(String text);
 }
